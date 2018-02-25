e328c141c03b077a4019f95f092e7ea7a97d560f
Steve Davis <deevodavis71@gmail.com>
2018-02-25 15:12:11 +0000

    Removed interface inheritance

--- src/main/java/com/sample/user/UserServiceApplication.java
+++ src/main/java/com/sample/user/UserServiceApplication.java
@@ -6,7 +6,6 @@ import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.spring.eventsourcing.SpringPrototypeAggregateFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 import com.sample.user.crypto.PBKDF2SaltedPasswordHashGenerator;
@@ -15,40 +14,31 @@ import com.sample.user.crypto.PasswordHashGenerator;
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
-	@Bean
-	@ConditionalOnMissingBean
-	public PasswordHashGenerator passwordHashGenerator() {
-		return new PBKDF2SaltedPasswordHashGenerator();
-	}
+    @Bean
+    //@ConditionalOnMissingBean
+    public PasswordHashGenerator passwordHashGenerator() {
+        return new PBKDF2SaltedPasswordHashGenerator();
+    }
 
+    @Bean
+    public AggregateFactory<UserAggregate> userAggregateFactory() {
 
+        SpringPrototypeAggregateFactory<UserAggregate> aggregateFactory = new SpringPrototypeAggregateFactory<>();
+        aggregateFactory.setPrototypeBeanName("userAggregate");
 
+        return aggregateFactory;
 
+    }
 
+    @Bean
+    public AggregateSnapshotter snapShotter(EventStore eventStore,
+                                            AggregateFactory<UserAggregate> bankAccountAggregateFactory) {
 
+        return new AggregateSnapshotter(eventStore, userAggregateFactory());
 
-
-
-
-	@Bean
-	public AggregateFactory<UserAggregate> userAggregateFactory() {
-
-		SpringPrototypeAggregateFactory<UserAggregate> aggregateFactory = new SpringPrototypeAggregateFactory<>();
-		aggregateFactory.setPrototypeBeanName("userAggregate");
-
-		return aggregateFactory;
-
-	}
-
-	@Bean
-	public AggregateSnapshotter snapShotter(EventStore eventStore,
-											AggregateFactory<UserAggregate> bankAccountAggregateFactory) {
-
-		return new AggregateSnapshotter(eventStore, userAggregateFactory());
-
-	}
+    }
 }
--- src/main/java/com/sample/user/crypto/SaltedPasswordHashGenerator.java
+++
@@ -1,5 +0,0 @@
-package com.sample.user.crypto;
-
-public interface SaltedPasswordHashGenerator extends PasswordHashGenerator {
-	public String generatePasswordHash(String password, byte[] salt);
-}
